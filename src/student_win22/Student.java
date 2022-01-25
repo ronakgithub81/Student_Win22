f972214e77a8d03eea4ec6bd3b5f327bb1b807eb
ronakgithub81 <46760686+ronakgithub81@users.noreply.github.com>
2022-01-25 09:29:59 -0500

    Update Student.java

--- src/student_win22/Student.java
+++ src/student_win22/Student.java
@@ -1,14 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Name: ROnak Sheth 
+ Date: 25th jan 2022
  */
 package student_win22;
 
-/**
- *
- * @author Ronak
- */
+
 public class Student {
     private String studentId;
     private String studentName;
